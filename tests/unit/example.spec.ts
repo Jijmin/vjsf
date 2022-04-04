61e217c2d48c370f14f0a3f7b5942cff845369dc
p_zzzzzzhou <p_zzzzzzhou@tencent.com>
2022-04-04 11:15:44 +0800

    feat: 正式开始单元测试补全

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -19,5 +19,12 @@ describe('JsonSchemaForm', () => {
     // 渲染了JsonSchemaForm组件，那可以找到NumberField节点
     const numberField = wrapper.findComponent(NumberField);
     expect(numberField.exists()).toBeTruthy();
+    // numberField中触发onChange会影响JsonSchemaForm中的value的变化
+    // await numberField.props('onChange')('123'); // 如果是使用第三方组件，不关注底层实现，可以这样测试
+    // numberField.props('onChange')并不是触发input的Input事件实现的
+    const input = numberField.find('input');
+    input.element.value = '123';
+    input.trigger('input');
+    expect(value).toBe(123);
   });
 });
